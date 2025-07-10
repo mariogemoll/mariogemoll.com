cedda7ba3a0998c74769d563c134479e05283afd
Mario Gemoll <mariogemoll@gmx.net>
2025-07-10 14:49:07 +0200

    Add a script to run all linting jobs

---
+++ scripts/checkts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Fail if a subcommand fails
+set -e
+
+# Print the commands
+set -x
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/../frontend
+
+npx eslint -c basic.eslint.config.js basic.eslint.config.js eslint.config.js
+npx eslint src
+
+npx tsc
+
+cd $SCRIPT_DIR/../builder
+
+npx eslint -c basic.eslint.config.js basic.eslint.config.js eslint.config.js
+npx eslint src
+
+npx tsc
+
--- scripts/makesite.sh
+++ scripts/makesite.sh
@@ -8,9 +8,9 @@ set -x
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR/frontend
+cd $SCRIPT_DIR/../frontend
 npx tsc
 
-cd $SCRIPT_DIR/builder
+cd $SCRIPT_DIR/../builder
 npx tsc
 node dist/makesite.js
