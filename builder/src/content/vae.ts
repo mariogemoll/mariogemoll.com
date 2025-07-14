b6249dc80bb9803aebd7cb47c061a96a9769ec24
Mario Gemoll <mariogemoll@gmx.net>
2025-07-14 17:47:55 +0200

    Add highlight.js

--- builder/src/content/vae.ts
+++ builder/src/content/vae.ts
@@ -1,4 +1,5 @@
 import fsExtra from 'fs-extra';
+import hljs from 'highlight.js';
 import MarkdownIt from 'markdown-it';
 import mathjax3 from 'markdown-it-mathjax3';
 import path from 'path';
@@ -8,7 +9,15 @@ import type { PageContentParams } from '../types.js';
 export async function generatePage(contentPath: string): Promise<PageContentParams> {
   const md = new MarkdownIt({
     html: true,
-    linkify: true
+    linkify: true,
+    highlight: (str, lang): string => {
+      if (lang && hljs.getLanguage(lang)) {
+        return `<pre><code class="hljs language-${lang}">` +
+          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+          '</code></pre>';
+      }
+      return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
   }).use(mathjax3);
 
   let mdContent = await fsExtra.readFile(path.join(contentPath, 'vae.md'), 'utf-8');
@@ -20,7 +29,10 @@ export async function generatePage(contentPath: string): Promise<PageContentPara
   }
 
   const html = md.render(mdContent);
-  const cssFiles = ['/vae/vae.css'];
+  const cssFiles = [
+    'https://cdn.jsdelivr.net/npm/highlight.js@11.9.0/styles/default.min.css',
+    '/vae/vae.css'
+  ];
   const jsUrls = ['https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js'];
   const jsModuleUrls = ['/vae/vae.js'];
   return [html, cssFiles, jsUrls, jsModuleUrls];
